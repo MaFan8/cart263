d0aa4a3ed3c97b58c8d7e44b4953de92cb9d821f
MaFan8 <maple_f@hotmail.com>
2021-01-19 18:53:20 -0500

    improved Where's Sausage Dog

--- activities/wheres-sausage-dog/js/script.js
+++ activities/wheres-sausage-dog/js/script.js
@@ -8,6 +8,9 @@ Find the sausage Dog **************************************************/
 const NUM_ANIMAL_IMAGES = 10;
 const NUM_ANIMALS = 100;
 
+const LOAD_ANIMAL_IMAGES = `assets/images/animal`;
+const LOAD_SAUSAGE_DOG_IMAGE = `assets/images/sausage-dog.png`;
+
 // place with empty array to store images
 let animalImages = [];
 let animals = [];
@@ -19,12 +22,12 @@ let sausageDog = undefined;
 function preload() {
   // load animals
   for (let i = 0; i < NUM_ANIMAL_IMAGES; i++) {
-    let animalImage = loadImage(`assets/images/animal${i}.png`); //'i' generates each number of animal
+    let animalImage = loadImage(`${LOAD_ANIMAL_IMAGES}${i}.png`); //'i' generates each number of animal
     animalImages.push(animalImage);
   }
 
   // load SausageDog
-  sausageDogImage = loadImage(`assets/images/sausage-dog.png`);
+  sausageDogImage = loadImage(`${LOAD_SAUSAGE_DOG_IMAGE}`);
 } //end PRELOAD
 
 // SETUP
@@ -32,32 +35,57 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 
   // create the animals
+  createAnimals();
+
+  // create sausageDog at random location
+  createSausageDog();
+} // END SETUP
+
+// create the animals
+function createAnimals() {
   for (let i = 0; i < NUM_ANIMALS; i++) {
-    let x = random(0, width);
-    let y = random(0, height);
-    let animalImage = random(animalImages);
-    let animal = new Animal(x, y, animalImage);
+    let animal = createRandomAnimal();
     animals.push(animal);
   }
+}
 
-  // create sausageDog in random location
+function createRandomAnimal() {
+  let x = random(0, width);
+  let y = random(0, height);
+  let animalImage = random(animalImages);
+  let animal = new Animal(x, y, animalImage);
+  return animal;
+}
+
+// create sausageDog at random location
+function createSausageDog() {
   let x = random(0, width);
   let y = random(0, height);
   sausageDog = new SausageDog(x, y, sausageDogImage);
-} // end SETUP
+}
 
 // DRAW
 function draw() {
   background(255, 255, 0);
 
   // call animals
+  updateAnimals();
+
+  // call sausageDog
+  updateSausageDog();
+} // END DRAW
+
+// call updated animals
+function updateAnimals() {
   for (let i = 0; i < animals.length; i++) {
     animals[i].update(); //give me animals at position 'i' and update it
   }
+}
 
-  // call sausageDog
+// call updated sausageDog
+function updateSausageDog() {
   sausageDog.update();
-} // end DRAW
+}
 
 function mousePressed() {
   sausageDog.mousePressed();
