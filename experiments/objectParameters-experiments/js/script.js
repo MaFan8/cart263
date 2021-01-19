21cd39eace25a0ddca240bec2606359681c13cfc
MaFan8 <maple_f@hotmail.com>
2021-01-19 13:05:23 -0500

    objectParameters-exp: finished
    -using config
    -destructure obj. parameters

--- experiments/objectParameters-experiments/js/script.js
+++ experiments/objectParameters-experiments/js/script.js
@@ -1,20 +1,40 @@
-/**************************************************
-Template p5 project
-Pippin Barr
+"use strict";
 
-Here is a description of this template p5 project.
-**************************************************/
-
-// setup()
-//
-// Description of setup() goes here.
 function setup() {
-
+  createCanvas(500, 500);
 }
 
-// draw()
-//
-// Description of draw() goes here.
 function draw() {
+  background(0);
 
+  // too many parameters!!
+  // drawFancyRect(250, 250, 200, 200, 255, 255, 0, CENTER);
+
+  let config = {
+    x: 250,
+    y: 250,
+    width: 200,
+    height: 200,
+    fillColor: {
+      r: 255,
+      g: 255,
+      b: 0,
+    },
+    mode: CENTER,
+  };
+  drawFancyRect(config);
+}
+
+// function drawFancyRect(x, y, w, h, r, g, b, mode) {
+// fill(r, b, b);
+//rectMode(mode);
+// rect(x, y, w, h);
+// }
+
+function drawFancyRect({ x, y, width, height, fillColor, mode }) {
+  push();
+  fill(fillColor.r, fillColor.g, fillColor.b);
+  rectMode(mode);
+  rect(x, y, width, height);
+  pop();
 }
