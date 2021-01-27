b24ee856b512cb818eb724c34be453779f05b171
MaFan8 <maple_f@hotmail.com>
2021-01-26 21:01:08 -0500

    Experiment: Annyang
    - boolean

--- experiments/annyang/js/script.js
+++ experiments/annyang/js/script.js
@@ -1,7 +1,32 @@
 "use strict";
 
-function preload() {}
+// Is the light on or off?
+let on = false;
 
-function setup() {}
+function setup() {
+  createCanvas(500, 500);
+  // Check if annyang is available
+  if (annyang) {
+    // Create commands
+    let commands = {
+      "Turn the light on": function () {
+        on = true;
+      },
+      "Turn the light off": function () {
+        on = false;
+      },
+    };
+    // Add the commands and start annyang
+    annyang.addCommands(commands);
+    annyang.start();
+  }
+}
 
-function draw() {}
+function draw() {
+  // If on is true, make the background white, otherwise make it black
+  if (on) {
+    background(255);
+  } else {
+    background(0);
+  }
+}
