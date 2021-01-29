798cb687f5c536a36ceaca219eca36f9cdca5e9e
MaFan8 <maple_f@hotmail.com>
2021-01-28 20:32:45 -0500

    Activity2: Slamina - set up responsive voice to say animal backwards - set up annyang to remember and listen to response

--- activities/Slamina/js/script.js
+++ activities/Slamina/js/script.js
@@ -1,7 +1,188 @@
 "use strict";
 
-function preload() {}
+const animals = [
+  "aardvark",
+  "alligator",
+  "alpaca",
+  "antelope",
+  "ape",
+  "armadillo",
+  "baboon",
+  "badger",
+  "bat",
+  "bear",
+  "beaver",
+  "bison",
+  "boar",
+  "buffalo",
+  "bull",
+  "camel",
+  "canary",
+  "capybara",
+  "cat",
+  "chameleon",
+  "cheetah",
+  "chimpanzee",
+  "chinchilla",
+  "chipmunk",
+  "cougar",
+  "cow",
+  "coyote",
+  "crocodile",
+  "crow",
+  "deer",
+  "dingo",
+  "dog",
+  "donkey",
+  "dromedary",
+  "elephant",
+  "elk",
+  "ewe",
+  "ferret",
+  "finch",
+  "fish",
+  "fox",
+  "frog",
+  "gazelle",
+  "gila monster",
+  "giraffe",
+  "gnu",
+  "goat",
+  "gopher",
+  "gorilla",
+  "grizzly bear",
+  "ground hog",
+  "guinea pig",
+  "hamster",
+  "hedgehog",
+  "hippopotamus",
+  "hog",
+  "horse",
+  "hyena",
+  "ibex",
+  "iguana",
+  "impala",
+  "jackal",
+  "jaguar",
+  "kangaroo",
+  "koala",
+  "lamb",
+  "lemur",
+  "leopard",
+  "lion",
+  "lizard",
+  "llama",
+  "lynx",
+  "mandrill",
+  "marmoset",
+  "mink",
+  "mole",
+  "mongoose",
+  "monkey",
+  "moose",
+  "mountain goat",
+  "mouse",
+  "mule",
+  "muskrat",
+  "mustang",
+  "mynah bird",
+  "newt",
+  "ocelot",
+  "opossum",
+  "orangutan",
+  "oryx",
+  "otter",
+  "ox",
+  "panda",
+  "panther",
+  "parakeet",
+  "parrot",
+  "pig",
+  "platypus",
+  "polar bear",
+  "porcupine",
+  "porpoise",
+  "prairie dog",
+  "puma",
+  "rabbit",
+  "raccoon",
+  "ram",
+  "rat",
+  "reindeer",
+  "reptile",
+  "rhinoceros",
+  "salamander",
+  "seal",
+  "sheep",
+  "shrew",
+  "silver fox",
+  "skunk",
+  "sloth",
+  "snake",
+  "squirrel",
+  "tapir",
+  "tiger",
+  "toad",
+  "turtle",
+  "walrus",
+  "warthog",
+  "weasel",
+  "whale",
+  "wildcat",
+  "wolf",
+  "wolverine",
+  "wombat",
+  "woodchuck",
+  "yak",
+  "zebra",
+];
 
-function setup() {}
+let currentAnimal = ``;
+let currentAnswer = ``;
 
-function draw() {}
+function setup() {
+  createCanvas (windowWidth, windowHeight);
+  if (annyang) {
+    let commands = {
+      'I think it is *animal': guessAnimal,
+    };
+    annyang.addCommands(commands);
+    annyang.start();
+
+    textSize(32);
+    textStyle(BOLD);
+    textAlign(CENTER, CENTER);
+  }
+}
+
+function draw() {
+  background(0);
+
+  if (currentAnswer === currentAnimal) {
+    fill(0,255,0);
+  } else {
+    fill(255,0,0);
+  }
+
+  text(currentAnswer, windowWidth/2, windowHeight/2);
+}
+
+
+function mousePressed() {
+  currentAnimal = random(animals);
+  let reverseAnimal = reverseString(currentAnimal);
+  responsiveVoice.speak(reverseAnimal);
+}
+
+
+// when annyang gets name of animal, it will pass into paramater of guessAnimal
+function guessAnimal(animal) {
+  currentAnswer = animal.toLowerCase();
+}
+
+function reverseString(string) {
+  let characters = string.split(``); //split string into array of characters
+  let reverseCharacters = characters.reverse(); //reverse the array of characters
+  let result = reverseCharacters.join(``); //join array of characters into a string
+  return result;
+}
