75a548c45c6545832e7e9a6c8a5840e7600194aa
MaFan8 <maple_f@hotmail.com>
2021-02-04 13:01:47 -0500

    EXP: JSON - URL loaded as JSON data, not file (random data in a file)

--- experiments/JSON-experiments/js/script.js
+++ experiments/JSON-experiments/js/script.js
@@ -1,58 +1,30 @@
 "use strict";
 
-let tarotData = undefined;
-let fortune = `No fortune found yet...`;
+let jokeText = ``; // The current joke.
+let jokeData = undefined; // The loaded joke data
 
 function preload() {
-// // immediate assigning results from tarotData
-//   tarotData = loadJSON(`assets/data/tarot_interpretations.json`);
-
-// loading from URL 
-tarotData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json`);
+  jokeData = loadJSON(`https://official-joke-api.appspot.com/jokes/programming/random`);
 }
-
-
 
 function setup() {
-  createCanvas (windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
 
-  // picks random card
-  let card = random(tarotData.tarot_interpretations);
-  fortune = random(card.fortune_telling);
+  // We get the joke object as the first element of the array
+  let joke = jokeData[0];
+  // Set the joke text as the setup and punchline properties together
+  jokeText = `${joke.setup}\n\n${joke.punchline}`;
 }
-
-
 
 function draw() {
-  background(225);
+  background(0);
 
-  // let firstShadowMeaning = tarotData.tarot_interpretations[0].meanings.shadow[0]; // displays 'Being gullible an d naive'
-  //
-  // push();
-  // textSize(32);
-  // textAlign(CENTER);
-  // fill(0);
-  // text(firstShadowMeaning, width/2, height/2);
-  // pop();
-
-
+  // Display the current joke
   push();
+  fill(255, 255, 0);
   textSize(32);
-  textAlign(CENTER);
-  fill(0);
-  text(fortune, width/2, height/2);
+  textAlign(CENTER, CENTER);
+  rectMode(CENTER);
+  text(jokeText, width / 2, height / 2, width / 2, height / 2);
   pop();
 }
-
-// function mousePressed() {
-//   // outside of preload, jSON file will be loaded, it will keep running until tarotLoaded is called
-//   loadJSON(`assets/data/tarot_interpretations.json`, tarotLoaded)
-// }
-//
-// // when it's loaded, then you can assign the data in perameters
-// function tarotLoaded(data) {
-//   tarotData = data; // saved data into tarotData
-//   // picks random card
-//   let card = random(tarotData.tarot_interpretations);
-//   fortune = random(card.fortune_telling);
-// }
