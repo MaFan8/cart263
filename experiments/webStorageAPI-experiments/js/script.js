e184a0ee37de3fd7268877d435ba024422a32bf5
MaFan8 <maple_f@hotmail.com>
2021-02-04 15:57:41 -0500

    EXP: Web Storage API - remember name

--- experiments/webStorageAPI-experiments/js/script.js
+++ experiments/webStorageAPI-experiments/js/script.js
@@ -1,59 +1,38 @@
 "use strict";
 
-let clicks = 0;
-
-// set score when loaded
-let gameData = {
-  highScore: 0
+let userData = {
+  name: `stranger`
 };
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  // load gameData
-  let data = JSON.parse(localStorage.getItem(`click-attack-game-data`));
-  // check if there is data, use it, otherwise use default = 0
-  if (data !== null) {
-    gameData = data;
+  //check if we know their name
+  let data = JSON.parse(localStorage.getItem(`web-storage-example-personalization`));
+  if (data) {
+    userData.name = data.name;
+  }
+  else {
+    //ask user to give name
+    userData.name = prompt(`What's your name?`);
+    // save data
+    localStorage.setItem(`web-storage-example-personalization`, JSON.stringify(userData));
   }
 }
-
-
 
 function draw() {
   background(255);
 
   push();
   textSize(64);
-  textAlign(CENTER, CENTER);
-  textStyle(BOLD);
-  fill(0);
-  text(clicks, width/2, height/2);
+  textAlign(CENTER);
+  text(`Hi there, ${userData.name}!`, width / 2, height / 2);
   pop();
-
-  push();
-  textSize(32);
-  textAlign(LEFT,TOP);
-  textStyle(BOLD);
-  fill(0);
-  text(`High score: ${gameData.highScore}`, 100, 100);
-  pop();
-
 }
 
-function mousePressed() {
-  clicks++;
 
-  // check if highScore has been beaten
-  if (clicks > gameData.highScore) {
-    gameData.highScore = clicks;
-    localStorage.setItem(`click-attack-game-data`, JSON.stringify(gameData)); // save converted data to string and updated score
-    }
-}
-
-// delete data
 function keyPressed() {
   if (key === `c`) {
-    localStorage.removeItem(`click-attack-game-data`);
+    localStorage.removeItem(`web-storage-example-personalization`);
   }
 }
