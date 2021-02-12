a462acb117ef0c7559c9914d5ae648134ce60cc0
MaFan8 <maple_f@hotmail.com>
2021-02-12 00:50:50 -0500

    E4: basic interaction + animation finished
    - update scissor display with handpose
    - set handpose position at a window to cut bubble string
    - stop handpose after string is cut and move bubble
    - added popZone
    -connected sound: 'blow' to speed up bubble + 'Clap' to try and pop bubble

--- exercises/E4-bubble_popper++/js/script.js
+++ exercises/E4-bubble_popper++/js/script.js
@@ -19,13 +19,18 @@ let handpose = undefined;
 
 let classifier;
 let soundModel =
-`https://teachablemachine.withgoogle.com/models/OMXUjyI3j/model.json`;
-// let options = {probabilityThreashold: 0.5};
-let label = 'listening...';
+  `https://teachablemachine.withgoogle.com/models/2msNc-lvl/model.json`;
+let option = {
+  probabilityThreashold: 0.7
+};
+let label = undefined;
 
 let string = undefined;
 let stringCut = false;
 let bubble = undefined;
+let bubblePopped = false;
+let popZone = undefined;
+let displayText = ``;
 
 let predictions = [];
 let scissorShow = true;
@@ -64,8 +69,8 @@ let bladeMiddleFingerBaseY = undefined;
 
 // PRELOAD
 function preload() {
-
-  classifier = ml5.soundClassifier(soundModel);
+  // load soundModel
+  classifier = ml5.soundClassifier(soundModel, option);
 } // END PRELOAD
 
 
@@ -94,24 +99,38 @@ function setup() {
 
   //string
   string = {
-    y: random(height/3, height/2),
-    fill: 255,
+    fill: (255),
     width: 1,
     height: height,
   };
   //bubble
   bubble = {
-    x: random(width),
-    // y: undefined,
-    y: random(height/3, height/2),
+    x: random(100, width - 100),
+    y: random(height / 3, height / 2),
     size: 80,
-    vx: random(-1,1),
+    vx: random(-1, 1),
     vy: -0.5,
     change: 0.02,
     speed: 1,
   };
+
+  // pop zone
+  popZone = {
+    x: random(100, width - 100),
+    y: random(100, height - 100),
+    size: 140,
+  };
+  resetpopZone();
+
 } // END SETUP
 
+function resetpopZone() {
+  let d = dist(bubble.x, bubble.y, popZone.x, popZone.y);
+  if (d < bubble.size/2 + popZone.size/2) {
+    popZone.x = random(100, width - 100);
+    popZone.y = random(100, height - 100);
+}
+}
 
 // DRAW
 function draw() {
@@ -121,12 +140,27 @@ function draw() {
   } else if (state === `running`) {
     running();
 
-    push();
-    fill(255);
-    textSize(32);
-    textAlign(CENTER, CENTER);
-    text(label, width / 2, height / 2);
-    pop();
+
+
+
+  if (label === `Blow`) {
+    bubble.vx += random(0, 1);
+    bubble.vy += random(0, 1);
+  }
+
+  let d = dist(bubble.x, bubble.y, popZone.x, popZone.y);
+  if (label === `Snap`) {
+      push();
+      fill(255);
+      textAlign(CENTER);
+      textSize(150);
+      text(`*`, popZone.x, popZone.y +80);
+      pop();
+
+      if (d < 50 && label === `Snap`){
+        bubblePopped = true;
+      }
+    }
 
   }
 } // END DRAW
@@ -148,34 +182,19 @@ function running() {
   if (predictions.length > 0 && scissorShow) {
     updateScissors(predictions[0]); // index and middle finger
 
-    if (bladeIndex.tip.y > bubble.y &&
-        bladeIndex.tip.y < (bubble.y + string.height) &&
-        bladeIndex.base.y > bubble.y &&
-        bladeIndex.base.y < (bubble.y + string.height)) {
-
-      let d = dist(bladeIndex.base.x, bladeIndex.base.y, bubble.x, bubble.y)
-      if (Math.floor(bladeIndex.tip.x) === Math.floor(bubble.x)) {
-        stringCut = true;
-      }
-    }
+    checkScissorCut();
     displayScissors();
   }
 
-
-  // display balloon on string
-  checkBounceOfWalls();
-  if (!stringCut) {
-  displayString();
-} else {
-    video.stop();
-    scissorShow = false;
-    moveBubble();
-
-    // // start listening for soundModel
-    // classifier.classify(gotResults);
+  checkStringIsCut(); // check string status
+  if (!bubblePopped) {
+  checkBounceOfWalls(); // bubble bounces within window
+  displayBubble();
 }
 
-  displayBubble();
+  displayPopZone();
+
+
 }
 
 function updateScissors(prediction) {
@@ -202,27 +221,68 @@ function updateScissors(prediction) {
     bladeMiddleFingerBaseY = bladeMiddleFinger.base.y;
 
     predictionStart = false;
-  }
-  else {
-  bladeIndexTipX = lerp(bladeIndexTipX, bladeIndex.tip.x, 0.5);
-  bladeIndexTipY = lerp(bladeIndexTipY, bladeIndex.tip.y, 0.5);
-  bladeIndexBaseX = lerp(bladeIndexBaseX, bladeIndex.base.x, 0.5);
-  bladeIndexBaseY = lerp(bladeIndexBaseY, bladeIndex.base.y, 0.5);
+  } else {
+    bladeIndexTipX = lerp(bladeIndexTipX, bladeIndex.tip.x, 0.5);
+    bladeIndexTipY = lerp(bladeIndexTipY, bladeIndex.tip.y, 0.5);
+    bladeIndexBaseX = lerp(bladeIndexBaseX, bladeIndex.base.x, 0.5);
+    bladeIndexBaseY = lerp(bladeIndexBaseY, bladeIndex.base.y, 0.5);
 
-  bladeMiddleFingerTipX = lerp(bladeMiddleFingerTipX, bladeMiddleFinger.tip.x, 0.5);
-  bladeMiddleFingerTipY = lerp(bladeMiddleFingerTipY, bladeMiddleFinger.tip.y, 0.5);
-  bladeMiddleFingerBaseX = lerp(bladeMiddleFingerBaseX, bladeMiddleFinger.base.x, 0.5);
-  bladeMiddleFingerBaseY = lerp(bladeMiddleFingerBaseY, bladeMiddleFinger.base.y, 0.5);
+    bladeMiddleFingerTipX = lerp(bladeMiddleFingerTipX, bladeMiddleFinger.tip.x, 0.5);
+    bladeMiddleFingerTipY = lerp(bladeMiddleFingerTipY, bladeMiddleFinger.tip.y, 0.5);
+    bladeMiddleFingerBaseX = lerp(bladeMiddleFingerBaseX, bladeMiddleFinger.base.x, 0.5);
+    bladeMiddleFingerBaseY = lerp(bladeMiddleFingerBaseY, bladeMiddleFinger.base.y, 0.5);
+  }
 }
+
+function checkScissorCut() {
+  // check if scissors are between string length
+  if (bladeIndex.tip.y > bubble.y + bubble.size / 2 &&
+    bladeMiddleFinger.tip.y > bubble.y + bubble.size / 2) {
+    // tips and bases of index finger surrounds bubble.x position
+    if ((bladeIndex.tip.x > bubble.x + bubble.size / 2 &&                 bladeIndex.base.x < bubble.x - bubble.size / 2) || (bladeIndex.tip.x < bubble.x - bubble.size / 2 &&
+    bladeIndex.base.x > bubble.x + bubble.size / 2)) {
+      // check if index + middle fingers are touching
+      let d = dist(bladeIndex.tip.x, bladeIndex.tip.y, bladeMiddleFinger.tip.x, bladeMiddleFinger.tip.y);
+      if (d < 25) {
+        stringCut = true;
+      }
+    }
+  }
 }
 
 function gotResult(error, results) {
   if (error) {
     console.error(error);
     return;
+  } else if (stringCut) {
+    console.log(results[0]);
+    label = results[0].label;
   }
-  // console.log(results[0]);
-  label = results[0].label;
+}
+
+function checkStringIsCut() {
+  if (!stringCut) {
+    displayString();
+  } else {
+    video.stop();
+    scissorShow = false;
+    moveBubble();
+  }
+}
+
+function checkBounceOfWalls() {
+  if (bubble.x + bubble.size / 2 > width) {
+    bubble.vx = -bubble.vx;
+  }
+  if (bubble.x - bubble.size / 2 < 0) {
+    bubble.vx = +0.5;
+  }
+  if (bubble.y + bubble.size / 2 > height) {
+    bubble.vy = -bubble.vy;
+  }
+  if (bubble.y - bubble.size / 2 < 0) {
+    bubble.vy = +0.5;
+  }
 }
 
 function moveBubble() {
@@ -230,27 +290,12 @@ function moveBubble() {
   bubble.y += bubble.vy;
 }
 
-function checkBounceOfWalls() {
-  if (bubble.x + bubble.size/2 > width) {
-    bubble.vx = -bubble.vx;
-  }
-  if (bubble.x - bubble.size/2 < 0) {
-    bubble.vx = +0.5;
-  }
-  if (bubble.y + bubble.size/2 > height) {
-    bubble.vy = -bubble.vy;
-  }
-  if (bubble.y - bubble.size/2 < 0) {
-    bubble.vy = +0.5;
-  }
-}
-
 function displayString() {
   push();
   noStroke();
   fill(string.fill);
   rectMode(CORNER);
-  rect(bubble.x+sin(frameCount/10), bubble.y, string.width, string.height);
+  rect(bubble.x + sin(frameCount / 10), bubble.y, string.width, string.height);
   pop();
 }
 
@@ -258,25 +303,37 @@ function displayBubble() {
   push();
   fill(0, 100, 200);
   noStroke();
-  ellipse(bubble.x, bubble.y,  bubble.size +sin(frameCount/5),
-  bubble.size +cos(frameCount/5));
+  ellipse(bubble.x, bubble.y, bubble.size + sin(frameCount / 5),
+    bubble.size + cos(frameCount / 5));
   pop();
 }
 
+function displayPopZone() {
+  push();
+  noFill();
+  stroke(128)
+  ellipse(popZone.x, popZone.y, popZone.size);
+  pop();
+
+}
+
 function displayScissors() {
+  let tipDist = dist(bladeIndex.tip.x, bladeIndex.tip.y, bladeMiddleFinger.tip.x, bladeMiddleFinger.tip.y);
+  let baseDist = dist(bladeIndex.base.x, bladeIndex.base.y, bladeMiddleFinger.base.x, bladeMiddleFinger.base.y);
+
   push();
   noFill();
   stroke(255);
   strokeCap(ROUND);
-  strokeWeight(15);
-  line(bladeIndexTipX, bladeIndexTipY, bladeIndexBaseX, bladeIndexBaseY);
-  line(bladeMiddleFinger.tip.x, bladeMiddleFinger.tip.y, bladeMiddleFinger.base.x, bladeMiddleFinger.base.y);
+  strokeWeight(5);
+  line(bladeIndexTipX, bladeIndexTipY + tipDist/2, bladeIndexBaseX, bladeIndexBaseY);
+  line(bladeMiddleFinger.tip.x, bladeMiddleFinger.tip.y, bladeIndex.base.x, bladeIndex.base.y);
   pop();
 
+  // hinge
   push();
   fill(255,0,0);
-  ellipseMode(CORNERS);
-  ellipse(bladeIndex.base.x, bladeIndex.base.y, bladeMiddleFinger.base.x, bladeMiddleFinger.base.y);
+  ellipseMode(CENTER);
+  ellipse(bladeIndex.base.x, bladeIndex.base.y, baseDist/2);
   pop();
-
 }
