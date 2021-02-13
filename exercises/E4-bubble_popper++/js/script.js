a84d328fa5364058c710abee94d7887d9850b5ad
MaFan8 <maple_f@hotmail.com>
2021-02-12 21:36:14 -0500

    E4: put bubble, popzone, and text in classes and updated script

--- exercises/E4-bubble_popper++/js/script.js
+++ exercises/E4-bubble_popper++/js/script.js
@@ -4,14 +4,22 @@
 Bubble Popper++
 Maple Sung
 
-Use hand to cut the ballon free, blow it around until it reaches the pop zone where user claps to pop the bubble.
+Use hand to cut the ballon free, blow it around until it reaches the pop zone where user snaps to pop the bubble.
 
 References:
 Based on Pippin Barr's Bubble Popper
 https://github.com/pippinbarr/cart263-2021/tree/main/examples/ai/bubble-popper/
+
+Uses:
+ml5.js Handpose
+https://learn.ml5js.org/#/reference/handpose
+Teachable Machine - Sound Classification
+https://teachablemachine.withgoogle.com/
+
+
 */
 
-let state = `running`; // loading, running
+let state = `running`; // start, running, end
 
 let video = undefined;
 let modelName = `Handpose`;
@@ -25,12 +33,10 @@ let option = {
 };
 let label = undefined;
 
-let string = undefined;
-let stringCut = false;
-let bubble = undefined;
-let bubblePopped = false;
-let popZone = undefined;
-let displayText = ``;
+let startText;
+let bubbleString;
+let bubble;
+let popZone;
 
 let predictions = [];
 let scissorShow = true;
@@ -77,124 +83,80 @@ function preload() {
 // SETUP
 function setup() {
   createCanvas(640, 480);
+
+  //Load handPose
+  loadHandPose();
+  // start listening for soundModel
+  classifier.classify(gotResult);
+
+  // load bubble + PopZone
+  bubble = new Bubble;
+  popZone = new PopZone;
+} // END SETUP
+
+function loadHandPose() {
   video = createCapture(VIDEO);
   video.hide();
 
-  // load handpose model
   handpose = ml5.handpose(video, {
     flipHorizontal: true
   }, function() {
-    state = `running`;
     console.log(`model loaded`);
+    // startText.displaySpace();
+    state = `running`;
   });
 
   // listen for predictions
   handpose.on('predict', function(results) {
     predictions = results;
-  })
-
-  // start listening for soundModel
-  classifier.classify(gotResult);
-
-
-  //string
-  string = {
-    fill: (255),
-    width: 1,
-    height: height,
-  };
-  //bubble
-  bubble = {
-    x: random(100, width - 100),
-    y: random(height / 3, height / 2),
-    size: 80,
-    vx: random(-1, 1),
-    vy: -0.5,
-    change: 0.02,
-    speed: 1,
-  };
-
-  // pop zone
-  popZone = {
-    x: random(100, width - 100),
-    y: random(100, height - 100),
-    size: 140,
-  };
-  resetpopZone();
-
-} // END SETUP
-
-function resetpopZone() {
-  let d = dist(bubble.x, bubble.y, popZone.x, popZone.y);
-  if (d < bubble.size/2 + popZone.size/2) {
-    popZone.x = random(100, width - 100);
-    popZone.y = random(100, height - 100);
-}
+  });
 }
 
 // DRAW
 function draw() {
 
-  if (state === `loading`) {
-    loading();
-  } else if (state === `running`) {
+  if (state === `start`) {
+    start();
+  }
+  else if (state === `running`) {
     running();
-
-
-
-
-  if (label === `Blow`) {
-    bubble.vx += random(0, 1);
-    bubble.vy += random(0, 1);
+  }
+  else if (state === `end`) {
+    end();
   }
 
-  let d = dist(bubble.x, bubble.y, popZone.x, popZone.y);
-  if (label === `Snap`) {
-      push();
-      fill(255);
-      textAlign(CENTER);
-      textSize(150);
-      text(`*`, popZone.x, popZone.y +80);
-      pop();
-
-      if (d < 50 && label === `Snap`){
-        bubblePopped = true;
-      }
-    }
-
-  }
 } // END DRAW
 
-
-function loading() {
-  push();
-  textSize(32);
-  textStyle(BOLD);
-  textAlign(CENTER, CENTER);
-  text(`Loading ${modelName}...`, width / 2, height / 2);
-  pop();
+function start() {
+  background(0);
+  startText = new StartText;
+  startText.displayTitle();
+  startText.display();
 }
 
 function running() {
   background(0);
-
-  // Check if there are current predictions (hand)
-  if (predictions.length > 0 && scissorShow) {
-    updateScissors(predictions[0]); // index and middle finger
-
-    checkScissorCut();
-    displayScissors();
-  }
-
+  checkHandPredictions(); // detect hand
   checkStringIsCut(); // check string status
-  if (!bubblePopped) {
-  checkBounceOfWalls(); // bubble bounces within window
-  displayBubble();
+  if (!bubble.popped) {
+    bubble.checkBounceOffWalls();
+    bubble.display();
+  }
+  checkSoundClassification(); // listen for specific sound
 }
 
-  displayPopZone();
+function end() {
+  background(243, 194, 5)
+  startText = new StartText;
+  startText.displayEnd();
+}
 
-
+function checkHandPredictions() {
+  if (predictions.length > 0 && scissorShow) {
+    updateScissors(predictions[0]); // index and middle finger
+    checkScissorCut(); // check for scissor cutting movement
+    displayScissors();
+  }
 }
 
 function updateScissors(prediction) {
@@ -239,12 +201,12 @@ function checkScissorCut() {
   if (bladeIndex.tip.y > bubble.y + bubble.size / 2 &&
     bladeMiddleFinger.tip.y > bubble.y + bubble.size / 2) {
     // tips and bases of index finger surrounds bubble.x position
-    if ((bladeIndex.tip.x > bubble.x + bubble.size / 2 &&                 bladeIndex.base.x < bubble.x - bubble.size / 2) || (bladeIndex.tip.x < bubble.x - bubble.size / 2 &&
-    bladeIndex.base.x > bubble.x + bubble.size / 2)) {
+    if ((bladeIndex.tip.x > bubble.x + bubble.size / 2 && bladeIndex.base.x < bubble.x - bubble.size / 2) || (bladeIndex.tip.x < bubble.x - bubble.size / 2 &&
+        bladeIndex.base.x > bubble.x + bubble.size / 2)) {
       // check if index + middle fingers are touching
       let d = dist(bladeIndex.tip.x, bladeIndex.tip.y, bladeMiddleFinger.tip.x, bladeMiddleFinger.tip.y);
       if (d < 25) {
-        stringCut = true;
+        bubble.stringIsCut = true;
       }
     }
   }
@@ -254,67 +216,36 @@ function gotResult(error, results) {
   if (error) {
     console.error(error);
     return;
-  } else if (stringCut) {
-    console.log(results[0]);
+  } else if (bubble.stringIsCut) {
+    // console.log(results[0]);
     label = results[0].label;
   }
 }
 
 function checkStringIsCut() {
-  if (!stringCut) {
-    displayString();
+  if (!bubble.stringIsCut) {
+    bubble.displayString();
   } else {
     video.stop();
     scissorShow = false;
-    moveBubble();
+    bubble.move();
+    popZone.display();
+    console.log(popZone.x, popZone.y);
   }
 }
 
-function checkBounceOfWalls() {
-  if (bubble.x + bubble.size / 2 > width) {
-    bubble.vx = -bubble.vx;
+function checkSoundClassification() {
+  if (label === `Blow`) {
+    bubble.blow();
   }
-  if (bubble.x - bubble.size / 2 < 0) {
-    bubble.vx = +0.5;
+  let d = dist(bubble.x, bubble.y, popZone.x, popZone.y);
+  if (label === `Snap`) {
+    popZone.displayText();
+
+    if (d < 50 && label === `Snap`) {
+      bubble.popped = true;
+    }
   }
-  if (bubble.y + bubble.size / 2 > height) {
-    bubble.vy = -bubble.vy;
-  }
-  if (bubble.y - bubble.size / 2 < 0) {
-    bubble.vy = +0.5;
-  }
-}
-
-function moveBubble() {
-  bubble.x += bubble.vx;
-  bubble.y += bubble.vy;
-}
-
-function displayString() {
-  push();
-  noStroke();
-  fill(string.fill);
-  rectMode(CORNER);
-  rect(bubble.x + sin(frameCount / 10), bubble.y, string.width, string.height);
-  pop();
-}
-
-function displayBubble() {
-  push();
-  fill(0, 100, 200);
-  noStroke();
-  ellipse(bubble.x, bubble.y, bubble.size + sin(frameCount / 5),
-    bubble.size + cos(frameCount / 5));
-  pop();
-}
-
-function displayPopZone() {
-  push();
-  noFill();
-  stroke(128)
-  ellipse(popZone.x, popZone.y, popZone.size);
-  pop();
-
 }
 
 function displayScissors() {
@@ -326,14 +257,20 @@ function displayScissors() {
   stroke(255);
   strokeCap(ROUND);
   strokeWeight(5);
-  line(bladeIndexTipX, bladeIndexTipY + tipDist/2, bladeIndexBaseX, bladeIndexBaseY);
+  line(bladeIndexTipX, bladeIndexTipY + tipDist / 2, bladeIndexBaseX, bladeIndexBaseY);
   line(bladeMiddleFinger.tip.x, bladeMiddleFinger.tip.y, bladeIndex.base.x, bladeIndex.base.y);
   pop();
 
   // hinge
   push();
-  fill(255,0,0);
+  fill(255, 0, 0);
   ellipseMode(CENTER);
-  ellipse(bladeIndex.base.x, bladeIndex.base.y, baseDist/2);
+  ellipse(bladeIndex.base.x, bladeIndex.base.y, baseDist / 2);
   pop();
+}
+
+function keyPressed() {
+  if (keyCode === 32) {
+    state = `running`;
+  }
 }
