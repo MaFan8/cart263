670fac7539a572910a18b269b9ce9a5570d36f99
MaFan8 <maple_f@hotmail.com>
2021-02-14 19:48:02 -0500

    Project01: create Head.js

---
+++ projects/project1-ace_inception/js/Head.js
@@ -0,0 +1,20 @@
+class Head {
+  constructor(x, y, image) {
+    this.x = x;
+    this.y = y;
+    this.image = image;
+    this.scale = 0.5;
+  }
+
+
+
+display() {
+  push();
+  imageMode(CENTER);
+  scale(this.scale);
+  translate(this.x, this.y);
+  image(this.image, 0, 0);
+  pop();
+}
+
+}
