1c2507cceb552272df8e30ca17e8bbafffca83f4
MaFan8 <maple_f@hotmail.com>
2021-02-19 23:16:00 -0500

    Project01: created User.js
     - move with FaceMesh prediction and display

---
+++ projects/project1-ace_inception/js/User.js
@@ -0,0 +1,37 @@
+class User {
+  constructor(image) {
+    this.image = image;
+    this.x = 0;
+    this.y = height - 80;
+    this.displayX = 0;
+    
+    this.active = true;
+  }
+
+  update(prediction) {
+    this.x = prediction.annotations.noseTip[0][0];
+    this.displayX = width - this.x; // flip horizontal
+  }
+
+
+  display() {
+    push();
+    imageMode(CENTER);
+    translate(this.displayX, this.y);
+    scale(0.7);
+    if (this.displayX < width / 2) {
+      scale(-1, 1);
+    }
+    image(this.image, 0, 0);
+    pop();
+  }
+
+  displayStatic() {
+    push();
+    imageMode(CENTER);
+    translate(width/2, this.y);
+    scale(0.7);
+    image(this.image, 0, 0);
+    pop();
+  }
+}
