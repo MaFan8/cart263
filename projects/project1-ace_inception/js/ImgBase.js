3b893158b626f8f5ec2fe4b1967ce87c7aaf59c0
MaFan8 <maple_f@hotmail.com>
2021-02-26 17:24:17 -0500

    Project01:  added vault in ImgBase.js
    - updated displayVault to rotate with user pose
    - added displayVaultStatic()

--- projects/project1-ace_inception/js/ImgBase.js
+++ projects/project1-ace_inception/js/ImgBase.js
@@ -9,6 +9,9 @@ class ImgBase {
     // heart
     this.stroke = (255);
     this.thickness = (10);
+    // vault
+    this.rotateSpeed = 0;
+    this.reCenter = 25;
 
     imageMode(CENTER);
   }
@@ -40,17 +43,29 @@ class ImgBase {
     pop();
   }
 
-  moveVault() {
-    
+  displayVaultStatic() {
+    push();
+    translate(this.x + 55, this.y - this.reCenter);
+    imageMode(CORNER);
+    tint(255, 80);
+    scale(this.scale);
+    image(this.image, 0, 0);
+    pop();
   }
 
-  displayVault() {
+  displayVault(user) {
     level_2Rect.push();
+    level_2Rect.translate(this.x, this.y +this.reCenter);
+    this.rotateSpeed += 0.02;
+    if (user.wristLY < height/2 && user.wristRY > height/2) {
+      level_2Rect.rotate(this.rotateSpeed);
+    } else if (user.wristLY > height/2 && user.wristRY < height/2) {
+      level_2Rect.rotate(-this.rotateSpeed);
+    }
     level_2Rect.imageMode(CENTER);
-    level_2Rect.translate(this.x, this.y);
     level_2Rect.tint(255, 80);
     level_2Rect.scale(this.scale);
-    level_2Rect.image(this.image, 0, 0);
+    level_2Rect.image(this.image, 0, -this.reCenter);
     level_2Rect.pop();
   }
 
@@ -59,7 +74,7 @@ class ImgBase {
     tint(255, 80);
     translate(video.width, 0);
     scale(-1, 1);
-    image(video, -310, 150, level_2Rect.width, level_2Rect.height)
+    image(video, -310, 150, level_2Rect.width, level_2Rect.height);
     pop();
   }
 
