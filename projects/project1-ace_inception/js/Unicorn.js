9cbb336cb8449813bcd4e5f018e3fb01d57508f0
MaFan8 <maple_f@hotmail.com>
2021-02-26 17:26:26 -0500

    Project01: Unicorn.js - added random unicorn images for background when in level_2
    -added: displayRandom(), moveRandom() to move and reset after leaving window

--- projects/project1-ace_inception/js/Unicorn.js
+++ projects/project1-ace_inception/js/Unicorn.js
@@ -3,20 +3,28 @@ class Unicorn {
     this.image = image;
     this.maxHeight = 90;
     this.alphaChange = -8;
+    this.istouched = false;
     this.reset();
     // for Ace only
     this.offScreen = 50;
     this.isPaused = false;
     this.startTime = 0;
     this.timePassed = 0;
+    // for random unicorns
+    this.randomX = random(width);
+    this.randomY = random(height);
+    this.velX = 0;
+    this.velY = 0;
+    this.vSpeed = 2;
+    this.jitterness = 0.01;
   }
 
   move() {
     // update position with velocity and image size increase
     this.x += this.vx;
     this.y += this.vy;
-    this.width += this.sizeIncrease/2;
-    this.height += this.sizeIncrease +(sin(frameCount/5));
+    this.width += this.sizeIncrease / 2;
+    this.height += this.sizeIncrease + (sin(frameCount / 5));
     this.tint += 2;
 
     // once image goes beyond 1/3 or 2/3 of window edges, then raise the image size and speed
@@ -35,6 +43,21 @@ class Unicorn {
     }
   }
 
+  moveRandom() {
+    let r = random();
+    if (r < this.jitterness) {
+      this.velX = random(-this.vSpeed, this.vSpeed);
+      this.velY = random(-this.vSpeed, this.vSpeed);
+    }
+    this.randomX = this.randomX + this.velX;
+    this.randomY = this.randomY + this.velY;
+
+    // reset animal if moved out of window
+    if (this.randomX < 0 || this.randomX > width || this.randomY < 0 || this.randomY > width) {
+      this.randomX = random(width);
+      this.randomY = random(height);
+    }
+  }
 
   // reset unicorn once it reaches window edge
   moveWrap() {
@@ -45,7 +68,7 @@ class Unicorn {
 
   // timer for unicronAce and resetting
   moveWrapAce() {
-    if (this.x < -this.offScreen || this.x > level_1Rect.width +this.offScreen || this.y > level_1Rect.height +this.offScreen) {
+    if (this.x < -this.offScreen || this.x > level_1Rect.width + this.offScreen || this.y > level_1Rect.height + this.offScreen) {
       if (!this.isPaused) {
         this.isPaused = true; // pause
         this.startTime = millis(); // start time
@@ -63,8 +86,8 @@ class Unicorn {
   }
 
   reset() {
-    this.x = random((level_1Rect.width/2 - 110), (level_1Rect.width/2 + 110));
-    this.y = level_1Rect.height/2;
+    this.x = random((level_1Rect.width / 2 - 150), (level_1Rect.width / 2 + 110));
+    this.y = level_1Rect.height / 2;
     this.width = random(5, 25);
     this.height = random(8, 33);
     this.tint = 100;
@@ -78,25 +101,35 @@ class Unicorn {
     this.vy = 0;
     this.speed = 1.2;
     this.sizeIncrease = 0.8;
+    this.istouched = false;
   }
 
   displayStatic() {
     push();
     imageMode(CENTER);
-    translate(250 + sin(frameCount * 0.05), height/2 + cos(frameCount * 0.05));
+    translate(250 + sin(frameCount * 0.05), height / 2 + cos(frameCount * 0.05));
     fill(216);
     noStroke();
-    ellipse(0,0,200);
     image(this.image, 0, 0);
     pop();
+  }
+
+  displayRandom() {
+    level_1Rect.push();
+    level_1Rect.imageMode(CENTER);
+    // level_1Rect.translate(250 + sin(frameCount * 0.05), height/2 + cos(frameCount * 0.05));
+    // level_1Rect.translate(this.randomX, this.randomY);
+    level_1Rect.scale(0.5);
+    level_1Rect.image(this.image, this.randomX, this.randomY);
+    level_1Rect.pop();
   }
 
   display() {
     level_1Rect.push();
     level_1Rect.imageMode(CENTER);
     level_1Rect.translate(this.x, this.y);
-    if (this.x < level_1Rect.width/2) {
-      level_1Rect.scale(-1,1);
+    if (this.x < level_1Rect.width / 2) {
+      level_1Rect.scale(-1, 1);
     } // flip x axis if unicorn is on right side
     level_1Rect.tint(this.tint, this.alpha);
     level_1Rect.image(this.image, 0, 0, this.width, this.height);
