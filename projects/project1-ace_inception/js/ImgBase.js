ed54e12edddbc708598fe48805ffabe6c52ef9d7
MaFan8 <maple_f@hotmail.com>
2021-03-02 16:21:17 -0500

    Project01: fixed displayVideo()
    - added level_2Rect.

--- projects/project1-ace_inception/js/ImgBase.js
+++ projects/project1-ace_inception/js/ImgBase.js
@@ -12,8 +12,10 @@ class ImgBase {
     // vault
     this.rotateSpeed = 0;
     this.reCenter = 25;
-
     this.movePosition = 100;
+    this.add = false;
+    this.subtract = false;
+
     // main parameters
     imageMode(CENTER);
   }
@@ -49,7 +51,7 @@ class ImgBase {
     level_2Rect.push();
     level_2Rect.translate(this.x, this.y);
     level_2Rect.imageMode(CENTER);
-    level_2Rect.tint(255, 80);
+    level_2Rect.tint(255, 60);
     level_2Rect.scale(this.scale);
     level_2Rect.image(this.image, 0, 0);
     level_2Rect.pop();
@@ -59,10 +61,13 @@ class ImgBase {
     level_2Rect.push();
     level_2Rect.translate(this.x, this.y +this.reCenter);
     this.rotateSpeed += 0.02;
-    if (user.wristLY < height/2 && user.wristRY > height/2) {
+    if (user.wristLY < level_2Rect.height/2 && user.wristRY > level_2Rect.height/2) {
       level_2Rect.rotate(this.rotateSpeed);
-    } else if (user.wristLY > height/2 && user.wristRY < height/2) {
+      this.add = true;
+    }
+    else if (user.wristLY > level_2Rect.height/2 && user.wristRY < level_2Rect.height/2) {
       level_2Rect.rotate(-this.rotateSpeed);
+      this.subtract = true;
     }
     level_2Rect.imageMode(CENTER);
     level_2Rect.tint(255, 80);
@@ -81,22 +86,23 @@ class ImgBase {
   }
 
   displayVideo() {
-    push();
-    tint(255, 80);
-    translate(video.width, 0);
-    scale(-1, 1);
-    image(video, -310, 150, level_2Rect.width, level_2Rect.height);
-    pop();
+    level_2Rect.push();
+    level_2Rect.tint(255, 60);
+    level_2Rect.imageMode(CORNER);
+    level_2Rect.translate(video.width, 0);
+    level_2Rect.scale(-1, 1);
+    level_2Rect.image(video, -60, 0, level_2Rect.width + 20, level_2Rect.height);
+    level_2Rect.pop();
   }
 
   displayFists(user) {
     level_2Rect.push();
-    level_2Rect.translate(user.wristLX, user.wristLY);
+    level_2Rect.translate(user.wristLX, user.wristLY - 100);
     level_2Rect.scale(this.scale);
     level_2Rect.image(this.image, 0,0);
     level_2Rect.pop();
     level_2Rect.push();
-    level_2Rect.translate(user.wristRX, user.wristRY);
+    level_2Rect.translate(user.wristRX, user.wristRY - 100);
     level_2Rect.scale(this.scale);
     level_2Rect.image(this.image, 0, 0);
     level_2Rect.pop();
