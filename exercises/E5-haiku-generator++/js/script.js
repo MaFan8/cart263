7e53cadf73249230b87c3fb5c7a95dc49eb4f138
MaFan8 <maple_f@hotmail.com>
2021-03-12 20:59:41 -0500

    E5: finished, added rita.js

--- exercises/E5-haiku-generator++/js/script.js
+++ exercises/E5-haiku-generator++/js/script.js
@@ -7,42 +7,51 @@ Generates random Haiku
 
 "use strict";
 
-
+let title = `Click to change the noun`
+let instruction = `Press "space" to restart`;
 let haikuLines = {
-fiveSyllables: [
-  `O, to be a tree`,
-  `The cat does not know`,
-  `We are all forests`,
-  `You have done your best`,
-  `They are all gone now`
-],
+  fiveSyllables: [
+    `O, to be a tree`,
+    `The cat does not know`,
+    `We are all forests`,
+    `You have done your best`,
+    `They are all gone now`
+  ],
 
-sevenSyllables: [
-  `Say the things left unsaid`,
-  `Never believe the wind's lies`,
-  `The autumn stretches its legs`,
-  `Nothing can satisfy you`,
-  `They will not come back again`
-]
+  sevenSyllables: [
+    `Say the things left unsaid`,
+    `Never believe the wind's lies`,
+    `The autumn stretches its legs`,
+    `Nothing can satisfy you`,
+    `They will not come back again`
+  ]
 };
 
+let lineTitle = document.getElementById(`title`);
+let lineInfo = document.getElementById('instruction');
 let line1 = document.getElementById(`line-1`);
 let line2 = document.getElementById(`line-2`);
 let line3 = document.getElementById(`line-3`);
 
 
 setupLines(); // Setup start lines
-addHoverListeners(); // listen for clicks and respond by changing them
-addClickListener();
-addKeyListener();
+addHoverListeners(); // listen for hovers and respond by changing them
+addClickListener(); // listen for clicks and respond by changing them
+addKeyListener(); // listen for key and respond by changing them
 
 function setupLines() {
+  lineTitle.innerText = title;
+  lineInfo.innerText = instruction;
   line1.innerText = random(haikuLines.fiveSyllables);
   line2.innerText = random(haikuLines.sevenSyllables);
   line3.innerText = random(haikuLines.fiveSyllables);
 }
 
 function addHoverListeners() {
+  lineTitle.addEventListener(`mouseenter`, highlight);
+  lineTitle.addEventListener(`mouseleave`, unHighlight);
+  lineInfo.addEventListener(`mouseenter`, highlight);
+  lineInfo.addEventListener(`mouseleave`, unHighlight);
   line1.addEventListener(`mouseenter`, highlight);
   line1.addEventListener(`mouseleave`, unHighlight);
   line2.addEventListener(`mouseenter`, highlight);
@@ -52,6 +61,7 @@ function addHoverListeners() {
 }
 
 let originalColor = haiku.style;
+
 function highlight(event) {
   event.target.style[`color`] = `#d7fcf4`;
 }
@@ -61,6 +71,8 @@ function unHighlight(event) {
 }
 
 function addClickListener() {
+  lineTitle.addEventListener(`click`, changeWord);
+  lineInfo.addEventListener(`click`, changeWord);
   line1.addEventListener(`click`, changeWord);
   line2.addEventListener(`click`, changeWord);
   line3.addEventListener(`click`, changeWord);
@@ -70,85 +82,107 @@ function changeWord(event) {
   fadeOut(event.target, 1);
 }
 
+
 function fadeOut(element, opacity) {
   opacity -= 0.01;
   element.style[`opacity`] = opacity; // set opacity on element
   if (opacity > 0) {
     // call fadeOut ONCE PER FRAME
     requestAnimationFrame(function() {
-      fadeOut(element,opacity);
+      fadeOut(element, opacity);
     });
-  }
-  else {
-    // do something when it's faded out...
+  } else if (element === lineInfo) {
+    changeVerb(element);
+    fadeIn(element, 0);
+  } else {
     changeNoun(element);
+    element.style[`color`] = `#5e1800`;
     fadeIn(element, 0);
   }
 }
 
+
 function fadeIn(element, opacity) {
   opacity += 0.01;
-  element.style[`opacity`] = opacity;  // set opacity on element
+  element.style[`opacity`] = opacity; // set opacity on element
   if (opacity < 1) {
     // call fadeOut ONCE PER FRAME
     requestAnimationFrame(function() {
-      fadeIn(element,opacity);
+      fadeIn(element, opacity);
     });
+  } else {
+    // do nothing
   }
-else {
-  // do nothing
-}
 }
 
-
+// Change noun
 function changeNoun(element) {
   let originalWord = element.innerText;
-    let words = RiTa.tokenize(element.innerText);
-    let partOfSpeech = RiTa.pos(element.innerText);
-    let output = " ";
-    let sentence = "";
-    for (let i = 0; i < words.length; i++) {
-    // if (/nn.*/.test(pos[i])) {
-    //   output = RiTa.randomWord({ targetPos: "nn"});
-    // }
+  let words = RiTa.tokenize(element.innerText); // break into strings of words
+  let partOfSpeech = RiTa.pos(element.innerText); // get parts of cpeech
+  let output = " "; // output is one word
+  let sentence = ""; // sentence of a string of words
+  // go through array in line
+  for (let i = 0; i < words.length; i++) {
+    // check if there are any nouns or plural nouns
     if (partOfSpeech[i] === "nn") {
-      output = RiTa.randomWord({ targetPos: "nn"});
-      words[i] = output;
-      // sentence = RiTa.untonkenize(words[i]);
+      //replace with noun
+      output = RiTa.randomWord({
+        targetPos: "nn"
+      });
+      words[i] = output; // word at index is the ouput
+      changed = true;
+    } else if (partOfSpeech[i] === "nns") {
+      // replace with plural noun
+      output = RiTa.randomWord({
+        targetPos: "nns"
+      });
+      words[i] = output; // word at index is the ouput
+      changed = true;
     }
-    else if (partOfSpeech[i] === "nns") {
-      output = RiTa.randomWord({ targetPos: "nns"});
-    }
-    else {
-      output = words[i];
-    }
-    console.log(output);
+    // put back into string of a sentence + space between them
+    sentence = sentence + words[i] + " ";
   }
+  // put sentence into element
+  element.innerText = sentence;
+}
 
-  console.log(partOfSpeech);
+// Change noun
+function changeVerb(element) {
+  let originalWord = element.innerText;
+  let words = RiTa.tokenize(element.innerText); // break into strings of words
+  let partOfSpeech = RiTa.pos(element.innerText); // get parts of cpeech
+  let output = " "; // output is one word
+  let sentence = ""; // sentence of a string of words
+  // go through array in line
+  for (let i = 0; i < words.length; i++) {
+    // check if there are any nouns or plural nouns
+    if (/vb.*/.test(partOfSpeech[i])) {
+      output = RiTa.randomWord({ targetPos: "vb"});
+      words[i] = output; // word at index is the ouput
+    }
+    // put back into string of a sentence + space between them
+    sentence = sentence + words[i] + " ";
+  }
+  // put sentence into element
+  element.innerText = sentence;
 }
 
 
 function addKeyListener() {
-  line1.addEventListener(`keyup`, changeLine);
-  line2.addEventListener(`keyup`, changeLine);
-  line3.addEventListener(`keyup`, changeLine);
+  document.addEventListener(`keyup`, changeLine);
 }
 
 function changeLine(event) {
   if (event.keyCode === 32) {
-  setNewLine(event.target);
-}
+    location.reload();
+  }
 }
 
-function setNewLine(element) {
-  if (element === line1 || element === line3) {
-    element.innerText = random(haikuLines.fiveSyllables);
-  }
-  else {
-    element.innerText = random(haikuLines.sevenSyllables);
-  }
-}
+// function setNewLine(element) {
+//   element.innerText = random(haikuLines.fiveSyllables);
+//   element.innerText = random(haikuLines.sevenSyllables);
+// }
 
 
 
