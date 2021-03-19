3c97b26e5760cb90534790d1d642072e6032059b
MaFan8 <maple_f@hotmail.com>
2021-03-19 16:26:40 -0400

    E6: Added title & instructions in html and css - got JSON file and display random objects in array and asssigned span onto them - erase random text and replaced with input field - check for correct/wrong answer

--- exercises/E6-erasure-mercenary/js/script.js
+++ exercises/E6-erasure-mercenary/js/script.js
@@ -1,34 +1,78 @@
 "use strict";
 
 const REVEAL_PROBABLILITY = 0.1;
-const UPDATE_FREQUENCY = 500;
+const ERASE_PROBABILITY = 0.05;
+const UPDATE_FREQUENCY = 2500;
+const SHAKESPEARE_URL = `https://raw.githubusercontent.com/dariusk/corpora/master/data/words/literature/shakespeare_phrases.json`;
+const NUM_LINES = 50;
 
 let $secrets;
+
 
 setup();
 // sets click handler and starts time loops
 function setup() {
-  $secrets = $(`.top-secret`);
-  $(`.top-secret`).on(`click`, redact);
+  $.getJSON(SHAKESPEARE_URL, displayJSON);
   setInterval(revelation, UPDATE_FREQUENCY);
 }
 
+function displayJSON(index, value) {
+  let lines = index.phrases; // get json array fo phrases
+  let outputLine = ""; // put into string
+  // go through array and take random lines
+  for (let i = 0; i < NUM_LINES; i++) {
+    let randomLine = Math.floor(lines.length * Math.random());
 
+    // randomly wrap span tags onto lines 12% of the time
+    let r = Math.random();
+    if (r < ERASE_PROBABILITY) {
+      outputLine = outputLine + "<span>" + lines[randomLine].trim() + "</span>" + ".\n";
+    } else {
+      outputLine = outputLine + lines[randomLine].trim() + ".\n";
+    }
+  }
 
-
-function redact() {
-  $(this).removeClass(`revealed`);
-  $(this).addClass(`redacted`);
+  $(`#paragraph-1`).html(outputLine); // add html elements onto 'p' tags
+  $(`#paragraph-1`).find($(`span`)).addClass(`revealed`); // add class to span
 }
 
 function revelation() {
-  $(`.redacted`).each(attemptReveal);
+  $(`.revealed`).each(attemptErase);
 }
 
-function attemptReveal() {
+function attemptErase() {
+  // replace text with an input field 10% of the time
   let r = Math.random();
   if (r < REVEAL_PROBABLILITY) {
-    $(this).removeClass(`redacted`);
-    $(this).addClass(`revealed`);
+    // change text color to red, then fadeOut
+    $(this).css('color', 'red').fadeOut(5000, function() {
+      // remove revealed class, change to text string
+      $(this).removeClass(`revealed`);
+      let textString = $(this).text();
+      $(this).text(``); // overwriting text to empty string
+      let $input = $(`<input type="text" data="${textString}">`);
+      $(this).replaceWith($input); // replace with input element
+
+      $input.keypress(enterInput); // allow 'enter' to store and replace text with input text
+    });
+  }
+}
+
+
+function enterInput() {
+  let keycode = event.keyCode;
+  // if ENTER is pressed
+  if (keycode === 13) {
+    // Check for correct text input
+    if ($(this).val() === $(this).attr("data")) {
+      console.log("correct");
+      //if correct, text will be blue
+      let userInput = "<span style = 'color:blue'>" + $(this).val() + "</span>";
+      $(this).replaceWith(userInput);
+    } else {
+      // if wrong, text will be red
+      let userInput = "<span style = 'color:red'>" + $(this).val() + "</span>";
+      $(this).replaceWith(userInput);
+    }
   }
 }
