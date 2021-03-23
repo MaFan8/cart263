e1399fef647b4505cc96fdf39716fe48185d9eae
MaFan8 <maple_f@hotmail.com>
2021-03-23 13:11:59 -0400

    Act: Code Taker Finished

--- activities/code-taker/js/script.js
+++ activities/code-taker/js/script.js
@@ -1,11 +1,39 @@
 /**
-Title of Project
-Author Name
+Code Taker
+Pippen Barr
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+The user is the Tom-Hanks-in-the-Da-Vinci-Code of classic poetry, seeing coded messages in poems. The user reads a poem and searches it with their mouse to uncover special letters. If they drag the letters in the correct order into a special solution area, they crack the code! Code taker! Da Vinci!?
 */
 
 "use strict";
 
-// Code goes here
+let secret = `Theremin`;
+
+$(`#solved-dialog`).dialog({
+  autoOpen: false,
+  buttons: {
+    "I know.": function() {
+      $(this).dialog(`close`);
+    }
+  }
+});
+
+$(`.secret`).one(`mouseover`, function(event) {
+  $(this).addClass(`found`, 500);
+  $(this).draggable({
+    helper: `clone`,
+  });
+});
+
+$(`#answer`).droppable({
+  drop: function(event, ui) {
+  let letter = ui.draggable.text();
+  $(this).append(letter); // add letter in answer box
+  ui.draggable.draggable(`disable`); // stop original letter from draggable
+  ui.draggable.removeClass(`found`, 500); //Stop highlighting
+  //Check if they got interval
+  if ($(this).text() === `Theremin`) {
+    $(`#solved-dialog`).dialog(`open`);
+  }
+}
+});
