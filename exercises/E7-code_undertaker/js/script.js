a250c906b612d9aa4d6712685edd3fb1e344559d
MaFan8 <maple_f@hotmail.com>
2021-03-23 23:19:52 -0400

    E7: Code Undertaker Finished - allowed jQuery UI sortable if enough letters are placed in answer box - check for right answer, color changes if correct answer and dialog box appears

--- exercises/E7-code_undertaker/js/script.js
+++ exercises/E7-code_undertaker/js/script.js
@@ -8,8 +8,22 @@ Code Taker by Pippin Barr
 */
 
 "use strict";
+let numLetters = 9;
+let secret = `stratagem`;
 
-let secret = `Theremin`;
+$(`#start-dialog`).dialog({
+  resizable: false,
+  modal: true,
+  buttons: {
+    "Begin": function() {
+      $(this).dialog(`close`);
+    }
+  },
+  hide: {
+    effect: "explode",
+    duration: 1000
+  }
+});
 
 $(`#solved-dialog`).dialog({
   autoOpen: false,
@@ -20,22 +34,56 @@ $(`#solved-dialog`).dialog({
   }
 });
 
+// mouseover secret letters turn red
 $(`.secret`).one(`mouseover`, function(event) {
   $(this).addClass(`found`, 500);
   $(this).draggable({
     helper: `clone`,
-  });
+  }); // clone dragged letter to replace in poem
 });
 
+// Dropping letters in answer box
 $(`#answer`).droppable({
   drop: function(event, ui) {
-  let letter = ui.draggable.text();
-  $(this).append(letter); // add letter in answer box
-  ui.draggable.draggable(`disable`); // stop original letter from draggable
-  ui.draggable.removeClass(`found`, 500); //Stop highlighting
-  //Check if they got interval
-  if ($(this).text() === `Theremin`) {
-    $(`#solved-dialog`).dialog(`open`);
+    // if original letter has class of secret,
+    if ($(ui.draggable[0]).hasClass("secret")) {
+      let letter = ui.draggable.text();
+      let liTag = $("<li>"); // create <li> tag
+      $(liTag).addClass("sortable-letter"); // add class to <li> tag
+      $(liTag).text(letter); // place letter in <li> tag
+      $("#sortable").append(liTag); // add <li> tag in answer box
+
+      ui.draggable.draggable(`disable`); // stop original letter from draggable
+      ui.draggable.removeClass(`found`, 500); //Stop highlighting
+    };
+
+    let word = $(`#sortable`).text(); // assign word to text from letters
+    let numLetter = word.split(``); // break down word to string
+    // if there is 9 letters, then allow sortable
+    if (numLetter.length >= numLetters) {
+      console.log("go");
+      $(`#sortable`).sortable({disabled: false});
+        $(this).css(`color`, `blue`);
+    } else {
+      $(`#sortable`).sortable({disabled: true});
+    }
   }
-}
+});
+
+// sorting letters in answer box
+$(`#sortable`).sortable({
+  stop: function(event, ui) {
+    let word = $(`#sortable`).text(); // assign word to letters in box
+    // if word is correct, turn background green, otherwise turn red
+    if (word === secret) {
+      $(this).css(`background-color`, `green`);
+      $(`#solved-dialog`).dialog(`open`);
+    } else {
+      $(this).css(`background-color`, `red`);
+    }
+  },
+  // change background to white when dragging
+  over: function(event, ui) {
+    $(this).css(`background-color`, `white`);
+  }
 });
